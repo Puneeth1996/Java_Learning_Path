4f9fa5a6bee47b86f1d155e0d29f099aabb47617
Puneeth1996 <puneeth1996p@gmail.com>
2022-03-27 12:54:32 +0530

    Created AuthorizationFilter and attached screenshots for working sample

--- SpringBoot/mobile-app-ws/src/main/java/com/developerblog/app/ws/security/WebSecurity.java
+++ SpringBoot/mobile-app-ws/src/main/java/com/developerblog/app/ws/security/WebSecurity.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -27,7 +26,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated()
-                .and().addFilter(getAuthenticationFilter());
+                .and()
+                .addFilter(getAuthenticationFilter())
+                .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
     @Override
