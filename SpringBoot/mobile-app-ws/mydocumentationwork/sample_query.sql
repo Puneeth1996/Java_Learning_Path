ae6c657d5709c2d249e45177554ffd4587d4aa12
Puneeth1996 <puneeth1996p@gmail.com>
2022-04-01 01:54:52 +0530

    Adding XML Support & JSON Support

---
+++ SpringBoot/mobile-app-ws/mydocumentationwork/sample_query.sql
@@ -0,0 +1,5 @@
+use photo_app;
+select * from users;
+delete from users;
+drop table users;
+describe users;
--- SpringBoot/mobile-app-ws/src/main/java/com/developerblog/app/ws/ui/controller/UserController.java
+++ SpringBoot/mobile-app-ws/src/main/java/com/developerblog/app/ws/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import com.developerblog.app.ws.ui.modal.request.UserDetailsRequestModal;
 import com.developerblog.app.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path="/{id}")
+    @GetMapping(path="/{id}",
+		produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 	public UserRest getUser(@PathVariable String id) {
 		
 		UserRest returnValue = new UserRest();		
@@ -27,7 +29,9 @@ public class UserController {
 	}
 	
 
-    @PostMapping
+	@PostMapping(
+			consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest createUser(@RequestBody UserDetailsRequestModal userDetails){
         UserRest returnValue = new UserRest();
 
